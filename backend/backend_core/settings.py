ba6a63a2b99115e984bf63a831215cd2ebf26155
Nandhini <nandhinib040@gmail.com>
2026-08-24 17:45:32 +0530

    Update

--- backend/backend_core/settings.py
+++ backend/backend_core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-bjfksidzdwd@7kr-c-)gko%btrfo_hd%xg8-ygm962k+uml2&v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
